da05ff3e508b87f77e8b950ada8374ae0bb86ce3
Gilad Peltz <gilad.peltz@gmail.com>
2021-02-10 12:56:58 +0200

    second commit still phase 1

--- script/main-controller.js
+++ script/main-controller.js
@@ -1,6 +1,36 @@
 'use strict';
 
-function onInit() {
-  console.log('onInit');
+var gElCanvas;
+var gCtx;
+var gCurrShape = 'text';
+var gCurrMeme;
+var gCurrImg;
 
+function onInit() {
+	console.log('onInit');
+	gElCanvas = document.getElementById('canvas');
+	gCtx = gElCanvas.getContext('2d');
+	gCtx.fillStyle = 'lightskyblue';
+	gCtx.fillRect(0, 0, gElCanvas.width, gElCanvas.height);
+}
+
+function onAddText(ev) {
+	ev.preventDefault();
+	var txt = document.querySelector('input[name=text]').value;
+	updateMemeText(txt);
+  gCtx.lineWidth = 2;
+	gCtx.strokeStyle = 'black';
+	gCtx.fillStyle = gCurrMeme.properties[0].color;
+	gCtx.font = `${gCurrMeme.properties[0].size}px ${gCurrMeme.properties[0].font}`;
+	gCtx.textAlign = gCurrMeme.properties[0].align;
+	gCtx.fillText(gCurrMeme.properties[0].txt, canvas.width / 2, 50);
+	gCtx.strokeText(gCurrMeme.properties[0].txt, canvas.width / 2, 50);
+}
+
+function onChooseImg(elImg) {
+	let img = elImg.src;
+	updateMemeImg(img);
+	gCurrMeme = getCurrMeme();
+	gCurrImg = getCurrImg();
+	gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height);
 }
--- script/main-services.js
+++ script/main-services.js
@@ -1,3 +1,30 @@
 'use strict';
 
-console.log('in main-services');
+// console.log('in main-services');
+
+const gKeywords = { happy: 12, 'funny puk': 1 };
+const gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['trump'] }];
+const gMeme = {
+	idx: 1,
+	selectedImgId: 5,
+	selectedLineIdx: 0,
+	properties: [
+		{ txt: 'Hola', img: 'img/1.jpg', size: 30, align: 'center', color: 'red', font: 'Impact' },
+	],
+  linesPos: {top: 50, mid: 250, bottom: 450}
+};
+
+function getCurrMeme() {
+	return gMeme;
+}
+function getCurrImg() {
+	return gImgs[0];
+}
+
+function updateMemeText(txt){
+  gMeme.properties[0].txt = txt;
+}
+
+function updateMemeImg(img){
+  gMeme.properties[0].img = img;
+}
